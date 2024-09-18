dc9d4490e3b0952e5ebe9c604fbd89b454cbb49d
Daniel Hangan <hangandaniel1@gmail.com>
2024-09-18 19:31:16 +0100

    remove next themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,15 +55,13 @@ export default function RootLayout({
       )}
     >
       <body className="antialiased max-w-3xl mx-4 mt-8 lg:mx-auto font-mono">
-        <ThemeProvider>
-          <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-            <Navbar />
-            {children}
-            <Footer />
-            <Analytics />
-            <SpeedInsights />
-          </main>
-        </ThemeProvider>
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+          <Navbar />
+          {children}
+          <Footer />
+          <Analytics />
+          <SpeedInsights />
+        </main>
       </body>
     </html>
   );
